f8b2b4afbeb11ae7237703a8459402720edb7603
Alex Noir <varsash@gmail.com>
2026-05-11 04:49:39 +0300

    Bump up version number to 5.5.0

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -70,10 +70,11 @@ class TsuserverDR:
         self._server = None  # Internal server object, changed to proper object later
 
         self.release = 5
-        self.major_version = 4
-        self.minor_version = 2
+        self.major_version = 5
+        self.minor_version = 0
         self.segment_version = ''
-        self.internal_version = '250628a'
+        # I wish someone explained to me what the "internal version" is supposed to be and how to calculate it :(
+        self.internal_version = '250700a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
